3b8c7599721d4bf865332c85d80f627c3068873d
Nataliia <nstasiukova@gmail.com>
2023-05-04 01:51:05 +0300

    Adding notification data dto

---
+++ src/main/java/parking/monitoring/NotificationData.java
@@ -0,0 +1,60 @@
+package parking.monitoring;
+
+public class NotificationData {
+
+	public long carNumber;
+	public String email;
+	public String name;
+	
+	public NotificationData(long carNumber, String email, String name) {
+		this.carNumber = carNumber;
+		this.email = email;
+		this.name = name;
+	}
+	
+	public NotificationData() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "NotificationData [carNumber=" + carNumber + ", email=" + email + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (carNumber ^ (carNumber >>> 32));
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NotificationData other = (NotificationData) obj;
+		if (carNumber != other.carNumber)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	
+	
+}
